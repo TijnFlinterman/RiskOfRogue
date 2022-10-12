16cac8f4ad63f4b81e349eb47e36d34e2e396d91
JimmevD <J.van.Duijnhoven@sintlucasedu.nl>
2022-10-12 15:01:01 +0200

    [#] Changed Movement Script [+] Sprite package

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -3,11 +3,14 @@ using UnityEngine;
 public class Player_Movement : MonoBehaviour
 {
     public float movementSpeed = 4f;
-    public Rigidbody2D rb;
-    Vector3 movespeed;
-    Vector3 moveSpeedUpDown;
+    private Rigidbody2D rb;
 
     private float horizontal, vertical;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     void Update()
     {
